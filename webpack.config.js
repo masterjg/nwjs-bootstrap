21183a964ee1e54c644cac753cbafacb9b69c157
Marius Guščius <masterj2@gmail.com>
2019-02-25 23:22:43 +0200

    dist folder + webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// @ts-ignore
+process.traceDeprecation = true;
+
+module.exports = { 
+  devtool: "source-map",
+  mode: process.env.NODE_ENV,
+  target: "node-webkit",
+  entry: [
+    path.resolve(__dirname, 'src/app.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.dist.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin([ path.resolve(__dirname, 'dist') ], {
+      exclude: [ '.gitkeep' ],
+      verbose: false
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      verbose: false
+    })
+  ]
+};
