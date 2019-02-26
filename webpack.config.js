46ef9444e1ccc2e8dec665934e426a2a792f6976
Marius Guščius <masterj2@gmail.com>
2019-02-27 00:37:27 +0200

    webpack-dev-server works but building doesnt now

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // @ts-ignore
 process.traceDeprecation = true;
+
+const mode = process.env.NODE_ENV || 'development';
+const isInDevMode = mode === 'development';
 
 module.exports = {
   stats: 'minimal',
@@ -11,23 +16,54 @@ module.exports = {
     "chrome": "chrome"
   },
   devtool: "source-map",
-  mode: process.env.NODE_ENV || 'development',
+  mode,
   target: "node-webkit",
-  entry: [
-    path.resolve(__dirname, 'src/app.js')
-  ],
+  entry: (() => {
+    const entries = [];
+    if (isInDevMode) {
+      entries.push(
+        'webpack-dev-server/client?http://127.0.0.1:8080',
+        'webpack/hot/only-dev-server',
+      );
+    }
+    entries.push(path.resolve(__dirname, 'src/app.js'));
+    return entries;
+  })(),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.dist.js'
   },
-  plugins: [
-    new CleanWebpackPlugin([ path.resolve(__dirname, 'dist') ], {
-      exclude: [ '.gitkeep' ],
-      verbose: false
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
-      filename: 'index.html'
-    })
-  ]
+  plugins: (() => {
+    const plugins = [
+      new CleanWebpackPlugin([ path.resolve(__dirname, 'dist') ], {
+        exclude: [ '.gitkeep' ],
+        verbose: false
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/index.html'),
+        filename: 'index.html'
+      }),
+      new CopyWebpackPlugin([
+        path.resolve(__dirname, 'src/icon.png')
+      ])
+    ];
+    if (isInDevMode) {
+      plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+      );
+    }
+    return plugins;
+  })(),
+  ...(() => {
+    return !isInDevMode ? {} : {
+      devServer: {
+        historyApiFallback: true,
+        hot: true,
+        stats: 'minimal',
+        watchContentBase: true,
+        contentBase: path.join(__dirname, 'src')
+      }
+    }
+  })()
 };
